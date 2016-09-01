6a8ec6f35f764c07694ff4a6905b5b703b33e0a4
Joe Turner <joe@oampo.co.uk>
2016-09-01 17:26:25 +0100

    Better technique with templating for sharing token.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+var fs = require('fs');
+
 var express = require('express');
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 var BearerStrategy = require('passport-http-bearer').Strategy;
+
+var secret = require('./secret');
 
 var app = express();
 
@@ -15,7 +19,7 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
     clientID:  '689026946763-rtsrhg52nra9oai4tk7gb05fs8f1t43l.apps.googleusercontent.com',
-    clientSecret: 'XXXXXXXXXXXXXXXXXXXX' ,
+    clientSecret: secret,
     callbackURL: "http://localhost:8080/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -42,8 +46,11 @@ app.get('/auth/google',
 app.get('/auth/google/callback', 
   passport.authenticate('google', {failureRedirect: '/login', session: false}),
   function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/logged-in.html?token=' + req.user.accessToken);
+    fs.readFile('public/logged-in.html', function(err, html) {
+        html = html.toString();
+        html = html.replace('{script}', '<script>var AUTH_TOKEN="' + req.user.accessToken + '"; history.replaceState(null, null, "/logged-in.html");</script>');
+        res.send(html);
+    });
 });
 
 app.get('/restricted', passport.authenticate('bearer', {session: false}), function(req, res) {
