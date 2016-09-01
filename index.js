9c0665a3e993f4fa01d96f0d8f85bd9d46df526b
Joe Turner <joe@oampo.co.uk>
2016-09-01 16:56:16 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var BearerStrategy = require('passport-http-bearer').Strategy;
+
+var app = express();
+
+var database = {
+};
+
+app.use(express.static('public'));
+app.use(passport.initialize());
+
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID:  '689026946763-rtsrhg52nra9oai4tk7gb05fs8f1t43l.apps.googleusercontent.com',
+    clientSecret: 'XXXXXXXXXXXXXXXXXXXX' ,
+    callbackURL: "http://localhost:8080/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      var user = database[accessToken] = {
+        googleId: profile.id,
+        accessToken: accessToken
+    };
+    return cb(null, user);
+  }
+));
+
+passport.use(new BearerStrategy(
+  function(token, done) {
+      if (!(token in database)) {
+          return done(null, false);
+      }
+      return done(null, database[token]);
+  }
+));
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', {failureRedirect: '/login', session: false}),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/logged-in.html?token=' + req.user.accessToken);
+});
+
+app.get('/restricted', passport.authenticate('bearer', {session: false}), function(req, res) {
+    return res.send("You got me");
+});
+
+app.listen(8080);
